17ecdcbcc0f9aa2d7b182304e5f32680ef181482
rmcedo <rafael9408@gmail.com>
2022-05-20 21:07:26 -0300

    Atualização da aula 20/05/2022 - 21:07

--- Orientacao a Objetos/src/br/com/entra21/orientacao/objetos/principal/main.java
+++ Orientacao a Objetos/src/br/com/entra21/orientacao/objetos/principal/main.java
@@ -7,26 +7,26 @@ import br.com.entra21.orientacao.objetos.principal.classes.Aluno;
 public class main {
 
 	static Scanner entrada = new Scanner(System.in);
+
 	public static void main(String[] args) {
-		
+
 		Aluno alunoTeste = new Aluno();
 		Aluno outroAluno = new Aluno();
 		Aluno alunoNovato = new Aluno();
 		Aluno alunoObjeto;
-		
+
 		alunoObjeto = new Aluno();
-		alunoObjeto.idade=18;
-		
-		alunoTeste.nome="Rafael";
-		alunoTeste.idade=27;
-		
-		outroAluno.idade=18;
-		outroAluno.nome="Visitante";
-		
+		alunoObjeto.idade = 18;
+
+		alunoTeste.nome = "Rafael";
+		alunoTeste.idade = 27;
+
+		outroAluno.idade = 18;
+		outroAluno.nome = "Visitante";
+
 		alunoTeste.responderChamada();
 		alunoTeste.responderChamada();
-		
-		
+
 		/*
 		 * System.out.println("Qual o nome do aluno novato? ");
 		 * alunoNovato.nome=entrada.next();
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Entra21_OrientacaoObjetos_2022
-Repositório que também será utilizado como Workspace no Eclipse
+
+> Repositório que também será utilizado como Workspace no Eclipse
+
+> Aqui constam os assuntos abordados em POO.
+
+> As aulas mesclam fundamentação teórica e práticas com exercícios
+
+![Gif Entra21](/entra21.gif)
+
